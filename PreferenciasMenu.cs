05ad1eb07ba7282b682b0d69972060eaba154263
luiscandidohonorio-student <78684232+luiscandidohonorio-student@users.noreply.github.com>
2021-02-23 17:39:07 -0300

    Add files via upload

---
+++ PreferenciasMenu.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PreferenciasMenu : MonoBehaviour
+{
+    public InputField ifScore;
+    public InputField ifLevel;
+    public Dropdown ddFase;
+    public Toggle tgSom;
+    public Slider slVolume;
+
+    public void SavePrefs() {
+
+        PlayerPrefs.SetString("Score", ifScore.text);
+        PlayerPrefs.SetInt("Level", int.Parse(ifLevel.text));
+        PlayerPrefs.SetInt("Fase", ddFase.value);
+        PlayerPrefs.SetString("Som", tgSom.isOn.ToString());
+        PlayerPrefs.SetFloat("Volume", slVolume.value);
+
+    }
+
+    public void LoadPrefs() {
+
+        ifScore.text = PlayerPrefs.GetString("Score");
+        ifLevel.text = PlayerPrefs.GetInt("Level", 1).ToString();
+        ddFase.value = PlayerPrefs.GetInt("Fase", 1);
+        tgSom.isOn = bool.Parse(PlayerPrefs.GetString("Som", true.ToString()));
+        slVolume.value = PlayerPrefs.GetFloat("Volume", 0.5f);
+
+    }
+
+    public void SetDefalts() {
+
+        PlayerPrefs.DeleteAll();
+        LoadPrefs();
+
+    }
+
+}
